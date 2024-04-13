445e326347530e32ec99219dd7d88f2227e4a77c
Artemis <Jonathanzhou8@gmail.com>
2024-04-13 12:30:53 -0700

    init commit

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="w-100 h-100 bg-red-400">silas</div>;
+}
+
+export default App;
