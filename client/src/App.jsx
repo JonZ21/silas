16022de587636360d9c624fec5e43b091b57c569
Jon <jonathanzhou8@gmail.com>
2024-04-13 13:28:24 -0700

    added mui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
 import { useState } from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import "./App.css";
 
 function App() {
-  return <div className="w-100 h-100 bg-blue-400">silas</div>;
+  return (
+    <div className="w-full h-screen bg-blue-400 flex flex-col items-center justify-center">
+      <div class="container">
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+      </div>
+      <div>
+        <h1 className="text-4xl font-bold text-white font-mono pt-10">
+          {" "}
+          Loading Silas
+        </h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
