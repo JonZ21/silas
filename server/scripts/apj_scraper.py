7001515171b6594e7a547ab8f7ad84b01bfafb9c
Jon <Jonathanzhou8@gmail.com>
2024-04-24 14:29:50 -0700

    summary made

--- server/scripts/apj_scraper.py
+++ server/scripts/apj_scraper.py
@@ -1,12 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 base_url = "https://www.desiringgod.org"
 episodes_url = f"{base_url}/ask-pastor-john"
-headers = {
-    "User-Agent": "Mozilla/5.0"
-}  # Add a User-Agent header to mimic a real browser
+headers = {"User-Agent": "Mozilla/5.0"}
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 
 def get_soup(url):
@@ -22,10 +28,23 @@ def scrape_episode_data(episode_url, episode_title):
     date = soup.find("time").text.strip()
     text = soup.find("div", class_="resource__body").text.strip()
 
-    print(date)
-    print(episode_number)
-    print(text)
-    print(episode_title)
+    # print(date)
+    # print(episode_number)
+    # print(text)
+    # print(episode_title)
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a Christian Bible Reading assistant. Given the title the audio transcript of a podcast episode, write a concise summary of the episode. Include all the verses referenced in the episode.",
+            },
+            {"role": "user", "content": f"{text}"},
+        ],
+    )
+
+    print("Content: " + completion.choices[0].message.content)
 
 
 def scrape_all_episodes():
@@ -39,7 +58,6 @@ def scrape_all_episodes():
             episode_url = base_url + href
             title = link.a.text.strip()
             episode_data = scrape_episode_data(episode_url, title)
-            print(episode_url)
             all_episodes.append(episode_url)
         # Check if there's a "more episodes" button and click it
         next_button = soup.find("a", {"id": "load_more_link"})
@@ -57,3 +75,13 @@ episodes_data = scrape_all_episodes()
 # scrape_episode_data("https://www.desiringgod.org/interviews/am-i-confident-or-arrogant")
 
 # Process episodes_data as needed (e.g., save to a file)
+
+
+# stream = client.chat.completions.create(
+#     model="gpt-3.5-turbo",
+#     messages=[{"role": "user", "content": "Say this is a test"}],
+#     stream=True,
+# )
+# for chunk in stream:
+#     if chunk.choices[0].delta.content is not None:
+#         print(chunk.choices[0].delta.content, end="")
